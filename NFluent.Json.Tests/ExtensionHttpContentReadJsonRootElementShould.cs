fd1fb4ff8a54bef2998b9f00b4d100b49dd74733
Yann D'Isanto <le-yams@users.noreply.github.com>
2025-05-06 11:35:49 +0200

    feat: read HttpContent as JsonElement utility method (#29)

---
+++ NFluent.Json.Tests/ExtensionHttpContentReadJsonRootElementShould.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using NFluent.Json.Extensions;
+using Xunit;
+
+namespace NFluent.Json.Tests;
+
+public class ExtensionHttpContentReadJsonRootElementShould
+{
+    [Fact]
+    public async Task ReadJsonElement()
+    {
+        // Arrange
+        var httpContent = JsonContent.Create(new
+        {
+            a = 1
+        });
+
+        // Act
+        var json = await httpContent.ReadJsonRootElementAsync();
+
+        // Assert
+        Check.That(json).HasIntProperty("a", 1);
+    }
+
+    [Fact]
+    public async Task FailWhenContentIsNull()
+    {
+        // Arrange
+        HttpContent? httpContent = null;
+
+        // Act
+        var thrown = await Assert.ThrowsAsync<ArgumentException>(() => httpContent.ReadJsonRootElementAsync());
+
+        // Assert
+        Check.That(thrown.Message).IsEqualTo("http content is null");
+    }
+
+    [Fact]
+    public async Task FailWhenWhenContentIsEmpty()
+    {
+        // Arrange
+        var httpContent = new StringContent(string.Empty);
+
+        // Act
+        var thrown = await Assert.ThrowsAsync<ArgumentException>(() => httpContent.ReadJsonRootElementAsync());
+
+        // Assert
+        Check.That(thrown.Message).IsEqualTo("http content is empty");
+    }
+}
---
+++ NFluent.Json/Extensions/HttpContentExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace NFluent.Json.Extensions;
+
+public static class HttpContentExtensions
+{
+    public static async Task<JsonElement> ReadJsonRootElementAsync(this HttpContent? content)
+    {
+        return (await content.ReadJsonDocumentAsync()).RootElement;
+    }
+
+    private static async Task<JsonDocument> ReadJsonDocumentAsync(this HttpContent? content)
+    {
+        content = content ?? throw new ArgumentException("http content is null");
+
+        var stringContent = await content.ReadAsStringAsync();
+        if (string.IsNullOrEmpty(stringContent))
+        {
+            throw new ArgumentException("http content is empty");
+        }
+
+        return JsonDocument.Parse(stringContent);
+    }
+}
